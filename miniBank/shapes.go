c0dad83706d51051cceb24facf60c8851e6dce87
youssef-joe <joe92228@gmail.com>
2025-11-17 14:11:20 +0200

    why using pointers ?

--- miniBank/shapes.go
+++ miniBank/shapes.go
@@ -1,5 +1,23 @@
 package wallet
 
+import "fmt"
+
 type Wallet struct {
-	Balance string
+	balance int
 }
+
+func (w *Wallet) Deposit(amount int) {
+	fmt.Printf("the address of balance in deposit is %p \n", &w.balance)
+	w.balance += amount
+}
+
+func (w *Wallet) Balance() int {
+	// why did not we make it return (*w).balance ** dereference ** ??
+	// the answer is that struct pointers in Go are automatically dereferenced 
+	return w.balance
+}
+
+// Technically you do not need to 
+// change Balance to use a pointer receiver as taking a copy of the balance is fine.
+//  However, by convention you should keep
+//   your method receiver types the same for consistency.
--- miniBank/wallet_test.go
+++ miniBank/wallet_test.go
@@ -1,12 +1,18 @@
 package wallet
 
-import "testing"
+import (
+	"testing"
+	"fmt"
+)
 
 func TestWallet(t *testing.T) {
 	wallet := Wallet{}
 	wallet.Deposit(10)
 
 	got := wallet.Balance()
+
+	fmt.Printf("the address of balance in test is %p \n", &wallet.balance)
+
 	want := 10
 
 	if got != want {
